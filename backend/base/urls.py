0c21c48b369d68a51426a81b03c6fb25eff33673
autsav <autsav73@.com>
2022-01-04 11:16:18 +0545

    User Serializer

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('products/', views.getProducts, name="products"),
     path('products/<str:pk>', views.getProduct, name="product"),
+    path('users/profile/', views.getUserProfile, name="user-profile"),
+
 
 ] 
--- backend/base/views.py
+++ backend/base/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 
 from .models import Product
 from .products import products
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer ,UserSerializer,UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -17,10 +17,10 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
 
-        data['username'] = self.user.username
-        data['email'] = self.user.email
-    
-
+        serializer = UserSerializerWithToken(self.user).data
+        
+        for key,value in serializer.items():
+            data[key] = value
         return data
 
 
@@ -45,6 +45,12 @@ def getRoutes(request):
         
     ]
     return Response(routes)
+
+@api_view(['GET'])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProducts(request):
