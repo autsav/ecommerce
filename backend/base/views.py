41381085c10853ccdaf1d847227db3bbbe40a358
autsav <autsav73@.com>
2022-01-04 23:50:39 +0545

    Register User

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.core.checks import messages
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework import serializers
@@ -12,6 +13,9 @@ from .serializers import ProductSerializer ,UserSerializer,UserSerializerWithTok
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
 # Create your views here.
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -29,24 +33,40 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
     
-@api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        '/api/products/',
-        '/api/products/create/',
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+    
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email = data['email'],
+            password = make_password(data['password'])      
+        )
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail':'User with this email already exist'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+# @api_view(['GET'])
+# def getRoutes(request):
+#     routes = [
+#         '/api/products/',
+#         '/api/products/create/',
         
-        '/api/products/upload/',
+#         '/api/products/upload/',
         
-        '/api/products/<id>/reviews/',
+#         '/api/products/<id>/reviews/',
         
-        '/api/products/top/',
-        '/api/products/<id>/',
+#         '/api/products/top/',
+#         '/api/products/<id>/',
         
-        '/api/products/delete/<id>/',
-        '/api/products/<update>/<id>/',
+#         '/api/products/delete/<id>/',
+#         '/api/products/<update>/<id>/',
         
-    ]
-    return Response(routes)
+#     ]
+#     return Response(routes)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
