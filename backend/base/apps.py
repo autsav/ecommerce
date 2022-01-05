cb413d312a32d4eec80d9c9800ac631a8c4cc765
autsav <autsav73@.com>
2022-01-05 14:09:50 +0545

    Login with email

--- backend/base/apps.py
+++ backend/base/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
+# from .signals import *
 
 
 class BaseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'base'
+    
+    def ready(self):
+        import base.signals
