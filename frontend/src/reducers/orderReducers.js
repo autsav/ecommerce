fbc211bb493f8e98a4f168c78687a417fd478a8c
autsav <autsav73@.com>
2022-02-28 18:50:14 +0545

    Order Details Reducer & Action

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -2,7 +2,11 @@ import {
     ORDER_CREATE_REQUEST,
     ORDER_CREATE_SUCCESS,
     ORDER_CREATE_FAIL,
-    ORDER_CREATE_RESET
+    ORDER_CREATE_RESET,
+
+    ORDER_DETAILS_REQUEST,
+    ORDER_DETAILS_SUCCESS,
+    ORDER_DETAILS_FAIL
  } from '../constants/orderConstants'
 
  export const orderCreateReducer = (state={}, action)=>{
@@ -28,3 +32,27 @@ import {
             return state
      }
  }
+
+ export const orderDetailsReducer = (state={loading:true, orderItems:[], shippingAddress:{}}, action)=>{
+    switch(action.type){
+        case ORDER_DETAILS_REQUEST:
+            return {
+                ...state,
+                loading:true
+            }
+       case ORDER_DETAILS_SUCCESS:
+           return {
+               loading:false,
+               success:true,
+               order:action.payload
+           }
+       case ORDER_DETAILS_FAIL:
+           return{
+               loading:false,
+               error:action.payload
+           }
+    
+       default:
+           return state
+    }
+}
