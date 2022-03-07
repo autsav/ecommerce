c0957d2c4e5a2cdab254497eda061345189d7ccb
autsav <autsav73@.com>
2022-03-07 12:23:09 +0545

    GET User By id & update user endpoint

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -69,16 +69,14 @@ def registerUser(request):
 @permission_classes([IsAuthenticated])
 def updateUserProfile(request):
     user = request.user
-    serializer = UserSerializer(user, many=False)
+    serializer = UserSerializerWithToken(user, many=False)
     
     data = request.data
     user.first_name = data['name']
     user.username = data['email']
     user.email = data['email']
     if data['password'] != '':
-        user.password = make_password(data['password'])
-          
-          
+        user.password = make_password(data['password'])          
     user.save()
 
     return Response(serializer.data)
@@ -97,6 +95,32 @@ def getUsers(request):
     serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUserById (request, pk):
+    user = User.objects.get(id=pk)
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUser(request, pk):
+    user = User.objects.get(id=pk)
+    
+    data = request.data
+    
+    user.first_name = data['name']
+    user.username = data['email']
+    user.email = data['email']
+    user.is_staff = data['isAdmin']
+            
+    user.save()
+    
+    serializer = UserSerializerWithToken(user, many=False)
+
+    return Response(serializer.data)
 
 
 @api_view(['DELETE'])
